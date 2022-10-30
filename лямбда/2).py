b8bf5ee1577802c532f637ff145408b448060197
zhiviza7331 <114140000+zhiviza7331@users.noreply.github.com>
2022-10-30 22:44:47 +0300

    Add files via upload

---
+++ лямбда/2).py
@@ -0,0 +1,5 @@
+grades = [{'name': 'Jennifer', 'final': 95},
+     {'name': 'David', 'final': 92},
+    {'name': 'Aaron', 'final': 98}]
+grades = sorted(grades, key=lambda grades: grades['name'])
+print(grades)
