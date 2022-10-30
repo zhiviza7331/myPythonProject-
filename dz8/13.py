99e4d2aa18a0b07dd9f08811ea3a58e84394eea0
zhiviza7331 <114140000+zhiviza7331@users.noreply.github.com>
2022-10-30 22:53:29 +0300

    Add files via upload

---
+++ dz8/13.py
@@ -0,0 +1,10 @@
+def a(b, c):
+    print('Sirius college')
+    print('Name: ', b)
+    print('Group: ', x)
+Count = int(input())
+for i in range(Count):
+    Name = input()
+    Group = input()
+    print(a(Name, Group))
+print('Готово! Заберите бэйджики')
